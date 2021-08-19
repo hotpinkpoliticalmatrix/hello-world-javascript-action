a14c2a3282a6466eafb719c6f0c3ae3c973b3ab7
Stacey Eliuk <54218699+hotpinkpoliticalmatrix@users.noreply.github.com>
2021-08-19 18:14:16 -0400

    changes

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const github = require("@actions/github")
 try {
  // `who-to-greet` input defined in action metadata file
  const currYear = new Date().getFullYear()
+
  const getWeek = (date = new Date()) => {
   const oneJan = new Date(date.getFullYear(), 0, 1)
   const numberOfDays = Math.floor((date - oneJan) / (24 * 60 * 60 * 1000))
   const result = Math.floor((date.getDay() + 1 + numberOfDays) / 7)
   return result
  }
+
  const currentBranch = core.getInput("current-branch")
  const staging = `**/release/${currYear}.${getWeek()}`
  const prod = `**/release/${currYear}.${getWeek() - 1}`
- console.log(currentBranch)
+ console.log({ currentBranch, staging, prod })
  //  core.setOutput("production-branch", prod)
  //  core.setOutput("staging-branch", staging)
 
