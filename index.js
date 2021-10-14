57230c3bb3208dd764853da75ca4acfd780ca381
Stacey Eliuk <54218699+hotpinkpoliticalmatrix@users.noreply.github.com>
2021-10-14 18:24:47 +0100

    refactoring script and edge case for year end

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
 
+const getWeek = (date = new Date()) => {
+ const oneJan = new Date(date.getFullYear(), 0, 1)
+ const numberOfDays = Math.floor((date - oneJan) / (24 * 60 * 60 * 1000))
+ const result = Math.floor((date.getDay() + 1 + numberOfDays) / 7)
+ return result
+}
+
+const adjustForYearEnd = (currWeek, currYear, stagingWeek, stagingYear) => {
+ if (currWeek === 52) {
+  stagingYear = currYear + 1
+  stagingWeek = 01
+ } else {
+  stagingYear = currYear
+  stagingWeek = result
+ }
+}
+
 try {
  //get current branch from input
  const currentBranch = core.getInput("current-branch")
- const getWeek = (date = new Date()) => {
-  const oneJan = new Date(date.getFullYear(), 0, 1)
-  const numberOfDays = Math.floor((date - oneJan) / (24 * 60 * 60 * 1000))
-  const result = Math.floor((date.getDay() + 1 + numberOfDays) / 7)
-  return result
- }
- const currYear = new Date().getFullYear()
+ let currYear = new Date().getFullYear()
+ let stagingWeek
+ let stagingYear
+
  const currWeek = getWeek()
+ adjustForYearEnd(currWeek, currYear, stagingWeek, stagingYear)
+
  const productionBranch = `release/${currYear}.${currWeek}`
- const stagingBranch = `release/${currYear}.${currWeek + 1}`
- const masterBranch = `release/${currYear}.${currWeek + 2}`
+ const stagingBranch = `release/${stagingYear}.${stagingWeek}`
  //to do: fix for december
  const isPushToStaging = currentBranch === stagingBranch
  const isPushToProd = currentBranch === productionBranch
- core.setOutput("masterBranch", masterBranch)
  core.setOutput("productionBranch", productionBranch)
  core.setOutput("stagingBranch", stagingBranch)
  core.setOutput("isPushToStaging", isPushToStaging)
