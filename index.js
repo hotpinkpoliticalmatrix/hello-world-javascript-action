c983faee5fa3bd03c11da03f3d0523f3b531484b
Stacey Eliuk <54218699+hotpinkpoliticalmatrix@users.noreply.github.com>
2021-10-14 17:22:16 +0100

    debugging script

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ try {
  core.setOutput("productionBranch", productionBranch)
  core.setOutput("stagingBranch", stagingBranch)
  core.setOutput("isPushToStaging", isPushToStaging)
- core.setOutput("isPushToProd", isPushToProd)
+ core.setOutput("isPushToProduction", isPushToProd)
+ console.log(productionBranch, isPushToProd)
 
  // Get the JSON webhook payload for the event that triggered the workflow
  //  const payload = JSON.stringify(github.context.payload, undefined, 2)
