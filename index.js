6b5e9ae277aefca314fc569d74210c0b891e6125
Stacey Eliuk <54218699+hotpinkpoliticalmatrix@users.noreply.github.com>
2021-10-14 14:30:50 +0100

    test for current year

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ try {
  const nameToGreet = core.getInput("who-to-greet")
  console.log(`Hello ${nameToGreet}!`)
  const time = new Date().toTimeString()
- core.setOutput("time", time)
+ const currYear = new Date().getFullYear()
+
+ core.setOutput("currYear", currYear)
  // Get the JSON webhook payload for the event that triggered the workflow
  const payload = JSON.stringify(github.context.payload, undefined, 2)
  console.log(`The event payload: ${payload}`)
