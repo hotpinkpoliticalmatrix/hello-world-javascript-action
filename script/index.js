992f35ce3fbfd14affb9a9d5e508cb02ab870670
Stacey Eliuk <54218699+hotpinkpoliticalmatrix@users.noreply.github.com>
2021-12-06 18:10:18 -0500

    gmm

--- script/index.js
+++ script/index.js
@@ -1,26 +1,26 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
 
-const getWeek = (date = new Date()) => {
- const oneJan = new Date(date.getFullYear(), 0, 1)
- const numberOfDays = Math.floor((date - oneJan) / (24 * 60 * 60 * 1000))
- const result = Math.floor((date.getDay() + 1 + numberOfDays) / 7)
- return result
-}
+// const getWeek = (date = new Date()) => {
+//  const oneJan = new Date(date.getFullYear(), 0, 1)
+//  const numberOfDays = Math.floor((date - oneJan) / (24 * 60 * 60 * 1000))
+//  const result = Math.floor((date.getDay() + 1 + numberOfDays) / 7)
+//  return result
+// }
 
-const adjustForYearEnd = (currWeek) => {
- let currYear = new Date().getFullYear()
- let stagingWeek
- let stagingYear
- if (currWeek === 52) {
-  stagingYear = currYear + 1
-  stagingWeek = 01
- } else {
-  stagingYear = currYear
-  stagingWeek = currWeek + 1
- }
- return { currYear, stagingWeek, stagingYear }
-}
+// const adjustForYearEnd = (currWeek) => {
+//  let currYear = new Date().getFullYear()
+//  let stagingWeek
+//  let stagingYear
+//  if (currWeek === 52) {
+//   stagingYear = currYear + 1
+//   stagingWeek = 01
+//  } else {
+//   stagingYear = currYear
+//   stagingWeek = currWeek + 1
+//  }
+//  return { currYear, stagingWeek, stagingYear }
+// }
 
 try {
  //get current branch from input
@@ -36,11 +36,11 @@ try {
  console.log(
   currentTag,
   currentTag.slice(0, 4),
-  currentTag.slice(5, 7),
-  productionBranch,
-  stagingBranch,
-  isPushToProduction,
-  isPushToStaging
+  currentTag.slice(5, 7)
+  // productionBranch,
+  // stagingBranch,
+  // isPushToProduction,
+  // isPushToStaging
  )
  core.setOutput("productionBranch", productionBranch)
  core.setOutput("stagingBranch", stagingBranch)
@@ -48,7 +48,7 @@ try {
  core.setOutput("isPushToProduction", isPushToProduction)
 
  // Get the JSON webhook payload for the event that triggered the workflow
- const payload = JSON.stringify(github.context.payload, undefined, 2)
+ //  const payload = JSON.stringify(github.context.payload, undefined, 2)
  //  console.log(`The event payload: ${payload}`)
 } catch (error) {
  core.setFailed(error.message)
