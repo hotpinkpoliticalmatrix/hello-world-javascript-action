a4b6e6e96433a13091f9512096d4cd2fbc3b8883
Stacey Eliuk <54218699+hotpinkpoliticalmatrix@users.noreply.github.com>
2021-12-06 18:36:26 -0500

    rm unused code

--- .github/actions/open-pr-from-hotfix/index.js
+++ .github/actions/open-pr-from-hotfix/index.js
@@ -1,29 +1,6 @@
 const core = require("@actions/core")
-const github = require("@actions/github")
-
-// const getWeek = (date = new Date()) => {
-//  const oneJan = new Date(date.getFullYear(), 0, 1)
-//  const numberOfDays = Math.floor((date - oneJan) / (24 * 60 * 60 * 1000))
-//  const result = Math.floor((date.getDay() + 1 + numberOfDays) / 7)
-//  return result
-// }
-
-// const adjustForYearEnd = (currWeek) => {
-//  let currYear = new Date().getFullYear()
-//  let stagingWeek
-//  let stagingYear
-//  if (currWeek === 52) {
-//   stagingYear = currYear + 1
-//   stagingWeek = 01
-//  } else {
-//   stagingYear = currYear
-//   stagingWeek = currWeek + 1
-//  }
-//  return { currYear, stagingWeek, stagingYear }
-// }
 
 try {
- //get current branch from input
  const currentBranch = core.getInput("current-branch")
  const currentTag = core.getInput("current-tag")
  const tagYear = currentTag.slice(0, 4)
@@ -47,10 +24,6 @@ try {
  core.setOutput("stagingBranch", stagingBranch)
  core.setOutput("isPushToStaging", isPushToStaging)
  core.setOutput("isPushToProduction", isPushToProduction)
-
- // Get the JSON webhook payload for the event that triggered the workflow
- //  const payload = JSON.stringify(github.context.payload, undefined, 2)
- //  console.log(`The event payload: ${payload}`)
 } catch (error) {
  core.setFailed(error.message)
 }
